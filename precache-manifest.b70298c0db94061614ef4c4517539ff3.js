a18a7334ecd0d3b8d59bab05b92dca8cdc2deab5
YuTingKung <davidkung007@gmail.com>
2020-04-13 19:24:57 +0800

    deploy

---
+++ precache-manifest.b70298c0db94061614ef4c4517539ff3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3873f1d99972c69fd7a6",
+    "url": "/css/app.c867fac5.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2dfee6475113325d2aa8bcc7e2bbc3ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c47cbc602c9f3019662c",
+    "url": "/js/about.8f1ac175.js"
+  },
+  {
+    "revision": "3873f1d99972c69fd7a6",
+    "url": "/js/app.746a5c7c.js"
+  },
+  {
+    "revision": "4443e9b5a1ff87a2c874",
+    "url": "/js/chunk-vendors.735d88dc.js"
+  },
+  {
+    "revision": "c045a5d5ac737316f15e72acb2f95b14",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
